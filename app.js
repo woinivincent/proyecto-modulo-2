1887b67fb7ee89578a7bd8c7063aa3b9d6aad783
Vicente Woinilowicz <139511901+woinivincent@users.noreply.github.com>
2023-11-28 20:31:00 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const sidebar = document.querySelector(".sidebar");
+const cross = document.querySelector(".bi-x-lg");
+const black = document.querySelector(".black");
+const sidebtn = document.querySelector(".second-1");
+
+sidebtn.addEventListener('click', () => {
+    sidebar.classList.add("active");
+    cross.classList.add("active");
+    black.classList.add("active");
+document.body.classList.add("stop-scroll");
+
+});
+cross.addEventListener('click', () => {
+    sidebar.classList.remove("active");
+    cross.classList.remove("active");
+    black.classList.remove("active");
+
+});
