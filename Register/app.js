e890d58c7334ba0143f8c484a8640197e7ac19da
Daniel777 <120663021+DanielSw777@users.noreply.github.com>
2023-12-05 21:36:03 -0300

    change: Integrando header y cards

--- Register/app.js
+++ Register/app.js
@@ -1,15 +1,16 @@
-
 const contBtn = document.querySelector('.button-link');
-const inputUser= document.querySelector('.login-name');
-const inputEmail= document.querySelector('.login-email');
-const inputPassword= document.querySelector('.login-password');
+const inputUser = document.querySelector('.login-name');
+const inputEmail = document.querySelector('.login-email');
+const inputPassword = document.querySelector('.login-password');
 const inputRePassword = document.querySelector('.login-re-password')
+
+
+
 contBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    console.log ("correcto");
-    console.log( inputUser.value);
+    console.log("correcto");
+    console.log(inputUser.value);
     console.log(inputEmail.value);
     console.log(inputPassword.value);
     console.log(inputRePassword.value);
-
 });
