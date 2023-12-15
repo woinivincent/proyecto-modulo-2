7697404eb9e45410117d982240d963a7a194d8ab
woinivincent <woinilowiczv@gmail.com>
2023-12-15 01:42:00 -0300

    se agregaron las funcionalidades a los formularios validaciones y redirecciones

--- Login/Login.js
+++ Login/Login.js
@@ -1,4 +1,24 @@
-const link = document.getElementById('returnHome')
-link.addEventListener('click', function () {
-    window.location.href = 'C:\Users\woini\Desktop\amazon\index.html';
-})
+document.querySelector('.form button').addEventListener('click', function (event) {
+    event.preventDefault(); 
+    validateLoginForm();
+});
+
+function validateLoginForm() {
+    var email = document.querySelector('.form input[name="email"]').value;
+
+    // Validación 
+    if (email === "") {
+        alert("Por favor, ingrese su correo electrónico o número de teléfono.");
+        return false;
+    }
+
+    // Validación del formato de correo electrónico 
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert("Ingrese un correo electrónico válido o número de teléfono.");
+        return false;
+    }
+
+    // Si la validación es exitosa, redirige a la página de inicio
+    window.location.href =  "/index.html";
+}
--- Register/app.js
+++ Register/app.js
@@ -1,16 +1,40 @@
-const contBtn = document.querySelector('.button-link');
-const inputUser = document.querySelector('.login-name');
-const inputEmail = document.querySelector('.login-email');
-const inputPassword = document.querySelector('.login-password');
-const inputRePassword = document.querySelector('.login-re-password')
 
+    document.getElementById('continueBtn').addEventListener('click', function () {
+        validateForm();
+    });
 
+    function validateForm() {
+        var name = document.getElementById('name').value;
+        var email = document.getElementById('mobile-email').value;
+        var password = document.getElementById('password').value;
+        var rePassword = document.getElementById('re-password').value;
 
-contBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log("correcto");
-    console.log(inputUser.value);
-    console.log(inputEmail.value);
-    console.log(inputPassword.value);
-    console.log(inputRePassword.value);
-});
+        // Validación  de todos los formularios
+        if (name === "" || email === "" || password === "" || rePassword === "") {
+            alert("Todos los campos son obligatorios");
+            return false;
+        }
+
+        // Validación del formato de correo electrónico
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            alert("Ingrese un correo electrónico válido");
+            return false;
+        }
+
+        if (password.length < 6) {
+            alert("La contraseña debe tener al menos 6 caracteres");
+            return false;
+        }
+
+        if (password !== rePassword) {
+            alert("Las contraseñas no coinciden");
+            return false;
+        }
+
+        // Si todos los campos son válidos, enviar formulario y redirigir a la home
+     
+       
+        window.location.href = "/index.html";
+    }
+
